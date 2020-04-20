00f2e5567747af8d1d6f2037f83c49da512f853c
MatthieuCoder <matthieu.p@unix-corp.tech>
2020-04-20 21:03:04 +0400

    resolve some issues with PsyKo new commit

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,17 +1,8 @@
 module.exports = {
-<<<<<<< HEAD
-  apps: [
-    {
-      name: 'webhook-server',
-      script: './src/app.js'
-    }
-  ]
-=======
 	apps: [
 		{
 			name: 'webhook-server',
 			script: './src/app.js'
 		}
 	]
->>>>>>> 73457ce0028716f3f609491ddf93ae9f37ef8a5c
 };
