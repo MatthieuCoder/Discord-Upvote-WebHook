841239bb130751a45d492e5578b60c05b66b137c
Daniel <37197986+SFallTech@users.noreply.github.com>
2020-04-20 17:51:53 +0300

    📦 Deleting src/index.js file, no longer used

--- src/index.js
+++
@@ -1,9 +0,0 @@
-const dotenv = require('dotenv');
-dotenv.config();
-
-const bluebird = require('bluebird');
-bluebird.config({ longStackTraces: true, warnings: { wForgottenReturn: false } });
-global.Promise = bluebird;
-
-const Server = require('./app');
-new Server();
