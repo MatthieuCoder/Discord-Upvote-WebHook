c688dee4134bd83783985a74fe236e547bf838e3
Daniel <37197986+SFallTech@users.noreply.github.com>
2020-04-20 17:44:19 +0300

    📦 Changing config.js file directory

---
+++ src/config.example.js
@@ -0,0 +1,17 @@
+module.exports = {
+	lists: [
+		{
+			name: 'Top.gg | Discord Bot List',
+			path: '/dbl',
+			token: '',
+			upvoteURL: 'top.gg/bot/BotID/vote',
+			icon: 'https://top.gg/images/dblnew.png'
+		}
+	],
+	webhook: {
+		id: '',
+		avatar: '',
+		name: '',
+		token: ''
+	}
+};
